1e4a1d907a2629d9022df02c7b8673f7a8fcb3d2
Uri Shaked <uri@salsa4fun.co.il>
2016-12-14 14:10:27 +0200

    Change input labels to "X" and "Y"
    They make more sense to me than X1, X2

--- src/playground.ts
+++ src/playground.ts
@@ -65,13 +65,13 @@ interface InputFeature {
 }
 
 let INPUTS: {[name: string]: InputFeature} = {
-  "x": {f: (x, y) => x, label: "X_1"},
-  "y": {f: (x, y) => y, label: "X_2"},
-  "xSquared": {f: (x, y) => x * x, label: "X_1^2"},
-  "ySquared": {f: (x, y) => y * y,  label: "X_2^2"},
-  "xTimesY": {f: (x, y) => x * y, label: "X_1X_2"},
-  "sinX": {f: (x, y) => Math.sin(x), label: "sin(X_1)"},
-  "sinY": {f: (x, y) => Math.sin(y), label: "sin(X_2)"},
+  "x": {f: (x, y) => x, label: "X"},
+  "y": {f: (x, y) => y, label: "Y"},
+  "xSquared": {f: (x, y) => x * x, label: "X^2"},
+  "ySquared": {f: (x, y) => y * y,  label: "Y^2"},
+  "xTimesY": {f: (x, y) => x * y, label: "X*Y"},
+  "sinX": {f: (x, y) => Math.sin(x), label: "sin(X)"},
+  "sinY": {f: (x, y) => Math.sin(y), label: "sin(Y)"}
 };
 
 let HIDABLE_CONTROLS = [
